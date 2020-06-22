a0a97700b48c22d19247a68cd13e383ed6a9ea3c
DigitalSoftMX <contacto@digitalsoft.mx>
2020-06-22 14:32:20 -0500

    Birthdate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration
             $table->char('sex');/*  */
             $table->string('phone')->nullable();/*  */
             $table->integer('active');
-            $table->string('birthdate');/*  */
+            $table->string('birthdate')->nullable();/*  */
             $table->string('password');/*  */
             $table->rememberToken();
             $table->timestamp('email_verified_at')->nullable();
