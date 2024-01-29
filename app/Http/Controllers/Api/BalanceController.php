8170d979c91a029193e4a4da96ddd9521f3ff677
Carlos RG <development@digitalquo.com>
2024-01-29 09:46:58 -0600

    Correction in sum to check (80 * )

--- app/Http/Controllers/Api/BalanceController.php
+++ app/Http/Controllers/Api/BalanceController.php
@@ -430,7 +430,7 @@ class BalanceController extends Controller
             }
         }
 
-        $points += $this->roundHalfDown($liters, $pointsEucomb); // Suma antes para verificar que la suma no sea mayor a (80 * $limit)
+        $points += $this->roundHalfDown($liters); // Suma antes para verificar que la suma no sea mayor a (80 * $limit)
         if ($points > (80 * $limit)) {
             $points -= $this->roundHalfDown($liters, $pointsEucomb);
             if ($points <= (80 * $limit)) {
